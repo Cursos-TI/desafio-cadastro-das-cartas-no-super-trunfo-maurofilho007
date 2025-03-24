f7c7b0ef33bce386208d2e57e6396d9475794ca7
maurofilho007 <mauroserex@gmail.com>
2025-03-24 02:34:37 +0000

    scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,23 @@
     int main(){
 
         printf("desafio super trunfo!\n");
+        int populacao;
+        int pontos;
+        float area;
+        float pib;
+        char nome[500];
+
+        printf("carta A01: \n");
+        printf("digite a populacao da carta: ");
+        scanf("%d", &populacao);
         
+
+        printf("populacao = %d\n", populacao);
+
+
+
+    
+
 
 
 
