196455088d4f636e0347b32269c42194a6815749
maurofilho007 <mauroserex@gmail.com>
2025-03-24 02:43:34 +0000

    scanf atuakizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,11 +10,28 @@
         char nome[500];
 
         printf("carta A01: \n");
-        printf("digite a populacao da carta: ");
+        printf("digite a populacao da carta:\n");
         scanf("%d", &populacao);
-        
+
+        printf("numero de pontos turisticos:\n");
+        scanf("%d", &pontos);
+
+        printf("qual area da cidade?:\n");
+        scanf("%f", &area);
+
+        printf("qual pib da cidade?:\n");
+        scanf("%f", &pib);
+
+
+
+
+
 
         printf("populacao = %d\n", populacao);
+        printf("pontos turisticos = %d\n", pontos);
+        printf("area = %f\n", area);
+        printf("pib = %f\n", pib);
+        
 
 
 
