14e6c230f16cc499db14c9914e8d53207fad2ba7
maurofilho007 <mauroserex@gmail.com>
2025-03-25 00:08:55 -0300

    new

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,43 +1,63 @@
 #include <stdio.h>
 
-    int main(){
+int main(){
 
-        printf("desafio super trunfo!\n");
-        int populacao;
-        int pontos;
-        float area;
-        float pib;
-        char nome[500];
+    printf("desafio super trunfo!\n");
+    int populacao;
+    int pontos;
+    float area;
+    float pib;
+    char nome[20];
 
-        printf("carta A01: \n");
-        printf("digite a populacao da carta:\n");
-        scanf("%d", &populacao);
+    printf("carta A01: ");
+    printf("digite a populacao da carta: ");
+    scanf("%d", &populacao);
 
-        printf("numero de pontos turisticos:\n");
-        scanf("%d", &pontos);
+    printf("numero de pontos turisticos: ");
+    scanf("%d", &pontos);
 
-        printf("qual area da cidade?:\n");
-        scanf("%f", &area);
+    printf("qual area da cidade?: ");
+    scanf("%f", &area);
 
-        printf("qual pib da cidade?:\n");
-        scanf("%f", &pib);
-
-
-
-
-
-
-        printf("populacao = %d\n", populacao);
-        printf("pontos turisticos = %d\n", pontos);
-        printf("area = %f\n", area);
-        printf("pib = %f\n", pib);
-        
-
-
-
-    
+    printf("qual pib da cidade?: ");
+    scanf("%f", &pib);
+    printf("\n \n");
 
 
 
 
-    }
+    printf("cartaA01: \n");
+    printf("populacao = %d\n", populacao);
+    printf("pontos turisticos = %d\n", pontos);
+    printf("area = %f\n", area);
+    printf("pib = %f\n", pib);
+
+    printf("\n \n");
+
+
+    printf("carta B02:\n");
+    printf("digite a populacao da carta:");
+    scanf("%d", &populacao);
+
+    printf("numero de pontos turisticos:");
+    scanf("%d", &pontos);
+
+    printf("qual area da cidade?:");
+    scanf("%f", &area);
+
+    printf("qual pib da cidade?:\n");
+    scanf("%f", &pib);
+
+
+    printf("carta B02:\n");
+    printf("populacao = %d\n", populacao);
+    printf("pontos turisticos = %d\n", pontos);
+    printf("area = %f\n", area);
+    printf("pib = %f\n", pib);
+
+
+    return 0;
+
+
+
+}
